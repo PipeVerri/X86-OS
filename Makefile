02ac9af3354f147bf384039dbd4ceffa4d92891b
felipe <pipe.verri@gmail.com>
2025-09-29 16:56:00 -0300

    Printing works

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ ASM = nasm # Usar nasm para compilar assembly, esto es solo una variable
 SRC_DIR = src
 BUILD_DIR = build
 
-main_floppy: $(BUILD_DIR)/main.bin
+main_floppy: main
 	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
 	truncate -s 1440k $(BUILD_DIR)/main_floppy.img # Padding con 0s para que quede del tamaño de un floppy disk
 
--- src/main.asm
+++ src/main.asm
@@ -3,21 +3,22 @@ bits 16
 
 %define ENDL 0x0D, 0x0A ; Macro para el newline
 
-msg: db 'Hello', ENDL, 0   ; Termino con el 0 para que no quede basura y siga imprimiendo
-
 main:
     ; Inicializar DS
     mov ax, 0
     mov ds, ax ; No puedo escribirle un numero directamente
     ; Inicializar stack
-    mov sp, 0x7c00  ; El stack es decremental y last-in-first-out, por ahora me alcanza el espacio
     mov ss, ax ; Limpiar el stack para que no vaya a cualquier lado
-
+    mov sp, 0x7c00  ; El stack es decremental y last-in-first-out, por ahora me alcanza el espacio
+    
     ; Llamar la funcion
     mov si, msg ; Seteo el offset en vez de DS ya que msg de escribe en DS al declararlo
     call print
-    
+
+.stop:
+    cli
     hlt
+    jmp .stop    
 
 
 ; Imprime a consola
@@ -31,7 +32,7 @@ print:
 
 .loop:
     lodsb   ; Carga el word en DS:SI en AX e incrementa SI para que DS:SI sea el siguiente word
-    or ax, ax   ; No modifica el caracter, pero si es nulo(todo 0) entonces setea ZF=1
+    or al, al   ; No modifica el caracter, pero si es nulo(todo 0) entonces setea ZF=1
     jz .done   ; Salta a done si ZF=1
 
     ; Imprimir el caracter
@@ -46,6 +47,7 @@ print:
     pop si
     ret
 
+msg: db 'Hello', ENDL, 0   ; Termino con el 0 para que no quede basura y siga imprimiendo
 
 times 510-($-$$) db 0 ; Un padding de 0s de 510 bytes - los usados para el programa
 dw 0AA55h ; El tag. Escrito asi porque NASM no entiende hexadecimal cuando empieza con una letra, y al reves por little endian
